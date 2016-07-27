200ea1a728050766850278b4e5120fb96af516c4
yceugenelai <eugene@bigobject.io>
2016-07-27 15:16:43 +0800

    use , delimiter

---
+++ pysetup2bo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "sh pysetup2bo.sh <bigobject container name>"
+	exit
+fi
+
+echo '#!/bin/bash\nsudo apt-get update \nsudo apt-get install python python-dev python-pip \nsudo pip install --upgrade pip \nsudo pip install numpy \nsudo pip install scipy' > ./pysetup.sh
+
+bo_docker_name=$1
+docker cp ./pysetup.sh $bo_docker_name:/
+docker exec bo bash -c "chmod +x /pysetup.sh ; /pysetup.sh"
+rm ./pysetup.sh
+docker cp * bo:/usr/local/bin
