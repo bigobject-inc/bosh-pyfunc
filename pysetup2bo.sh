92d17917dcdd9c90581a1e516d0d247a426d6dad
yceugenelai <eugene@bigobject.io>
2016-07-27 15:34:53 +0800

    update sh

--- pysetup2bo.sh
+++ pysetup2bo.sh
@@ -6,10 +6,10 @@ if [ -z "$1" ]
 	exit
 fi
 
-echo '#!/bin/bash\nsudo apt-get update \nsudo apt-get install python python-dev python-pip \nsudo pip install --upgrade pip \nsudo pip install numpy \nsudo pip install scipy' > ./pysetup.sh
+echo '#!/bin/bash\nsudo apt-get update \nsudo apt-get install -y python python-dev python-pip \nsudo pip install --upgrade pip \nsudo pip install numpy \nsudo pip install scipy' > ./pysetup.sh
 
 bo_docker_name=$1
 docker cp ./pysetup.sh $bo_docker_name:/
 docker exec bo bash -c "chmod +x /pysetup.sh ; /pysetup.sh"
 rm ./pysetup.sh
-docker cp * bo:/usr/local/bin
+docker cp . bo:/usr/local/bin
