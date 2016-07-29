b1e29a94ccce95a275fc15b42e6e61bbcbbe7621
yceugenelai <eugene@bigobject.io>
2016-07-29 17:17:21 +0800

    script fix

--- pysetup2bo.sh
+++ pysetup2bo.sh
@@ -6,10 +6,13 @@ if [ -z "$1" ]
 	exit
 fi
 
-echo '#!/bin/bash\nsudo apt-get update \nsudo apt-get install -y python python-dev python-pip \nsudo pip install --upgrade pip \nsudo pip install numpy \nsudo pip install scipy \nsudo pip install pandas' > ./pysetup.sh
+echo '#!/bin/bash\nsudo apt-get update \nsudo apt-get install -y python python-numpy python-scipy python-pandas' > ./pysetup.sh
 
 bo_docker_name=$1
 docker cp ./pysetup.sh $bo_docker_name:/
-docker exec bo bash -c "chmod +x /pysetup.sh ; /pysetup.sh"
+docker exec $bo_docker_name bash -c "chmod +x /pysetup.sh ; /pysetup.sh"
 rm ./pysetup.sh
-docker cp . bo:/usr/local/bin
+docker cp ./addConcatCol $bo_docker_name:/usr/local/bin
+docker cp ./getKmeanCent $bo_docker_name:/usr/local/bin
+docker cp ./getKmeanLabel $bo_docker_name:/usr/local/bin
+docker cp ./kmean $bo_docker_name:/usr/local/bin
