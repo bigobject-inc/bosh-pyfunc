5c7d28e5ca10e538122662ccd4426ab5d915e32e
yceugenelai <eugene@bigobject.io>
2016-08-24 11:21:12 +0800

    add eval

--- pysetup2bo.sh
+++ pysetup2bo.sh
@@ -20,3 +20,4 @@ docker cp ./pandas $bo_docker_name:/usr/local/bin
 docker cp ./pandas_print $bo_docker_name:/usr/local/bin
 docker cp ./remote $bo_docker_name:/usr/local/bin
 docker cp ./distance $bo_docker_name:/usr/local/bin
+docker cp ./eval $bo_docker_name:/usr/local/bin
